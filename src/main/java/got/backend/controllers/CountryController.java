15a223cae641f3651d8d443986d1252a7a39f46a
Dejan Tosenberger <tosemberger1@gmail.com>
2021-03-19 16:53:38 +0100

    Ispravljene greske u controleru

--- src/main/java/got/backend/controllers/CountryController.java
+++ src/main/java/got/backend/controllers/CountryController.java
@@ -4,11 +4,14 @@ import got.backend.model.Country;
 import got.backend.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@CrossOrigin
 public class CountryController {
     @Autowired
     private CountryRepository countryRepository;
